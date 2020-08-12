aa33c4373598d7d631d0569ff6a00251f382fc88
KaoutarOULAHYANE <Kaoutar.oulahyane@gmail.com>
2020-08-13 00:11:29 +0100

    comit Abonne

--- src/main/java/com/example/demande_chequier/metier/AppRole.java
+++ src/main/java/com/example/demande_chequier/metier/AppRole.java
@@ -3,18 +3,21 @@ package com.example.demande_chequier.metier;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class AppRole {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String roleNom;
 }
