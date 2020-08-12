c123e6a1f648c6670e62fed6f71025cc675f0dca
KaoutarOULAHYANE <Kaoutar.oulahyane@gmail.com>
2020-08-12 21:43:00 +0100

    comit AppRole.java

---
+++ src/main/java/com/example/demande_chequier/metier/AppRole.java
@@ -0,0 +1,23 @@
+package com.example.demande_chequier.metier;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppRole {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String roleName;
+
+
+}
